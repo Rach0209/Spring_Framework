5fd0877dfa16c2fc21116b13b3b6c0784a12237c
you_tu <104557592+Rach0209@users.noreply.github.com>
2022-09-07 09:18:13 +0900

    Add files via upload

---
+++ workspace-sts/hello/src/test/java/kr/co/greenart/JdbcTemplateTest.java
@@ -0,0 +1,27 @@
+package kr.co.greenart;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import kr.co.greenart.config.RootConfig;
+
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes = { RootConfig.class } )
+public class JdbcTemplateTest {
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+	
+	@Test
+	public void test() {
+//		결과값이 하나인 쿼리문을 줄 수있다.
+		int result = jdbcTemplate.queryForObject("SELECT 1", int.class);
+		
+		assertEquals(1, result);
+	}
+}
